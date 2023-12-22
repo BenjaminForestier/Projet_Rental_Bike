d84502e4750dc0c3249534e18c20d2b55294ef71
BenjaminF <benji-fo@outlook.fr>
2023-12-22 11:44:36 +0100

    slider

--- views/home.php
+++ views/home.php
@@ -1,67 +1,60 @@
-<?php 
-// var_dump($_SESSION)
+<?php
+    // var_dump($_SESSION)
 ;
-ob_start()?>
+ob_start() ?>
 <section id="banner">
-                <div class = "banner__text ">
-                    <h2>Lorem Ipsum</h2>
-                    <h3> <span>Lorem ipsum dolor,</span> sit amet <br>consectetur adipisicing elit Repudiandae.<br> Repudiandae <span>consequuntur amet.</span></h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur</p>
-                    <a class="btn btn-success w-50" href="?path=bikes.index">Réserver</a>
-                </div>
-                <div class="banner_img">
-                    <img src="./assets/img/7732616_5243.svg" alt="" width="650px">
-                </div>
-        </section>
-        <section id="gallery_slide">
-            <h2 class="text-center last_model_title">Derniers modèles</h2>
-            <div class="slider">
-                <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike2.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike3.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike4.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike5.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike6.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike7.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike8.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike9.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike10.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike11.jpg" alt="" srcset="" width="300vw">
-                <img src="./assets/img/bike12.jpg" alt="" srcset="" width="300vw">
-            </div>
-            <!-- <div class="swiper mySwiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <img src="./assets/img/velo1.jpg" alt="">
-                        </div>
-                        <div class="swiper-slide">
-                            -->
+    <div class="banner__text ">
+        <h2>Lorem Ipsum</h2>
+        <h3> <span>Lorem ipsum dolor,</span> sit amet <br>consectetur adipisicing elit Repudiandae.<br> Repudiandae <span>consequuntur amet.</span></h3>
+        <p>Lorem ipsum dolor, sit amet consectetur</p>
+        <a class="btn btn-success w-50" href="?path=bikes.index">Réserver</a>
+    </div>
+    <div class="banner_img">
+        <img src="./assets/img/7732616_5243.svg" alt="" width="650px">
+    </div>
+</section>
+<section id="gallery_slide">
+    <h2 class="text-center last_model_title">Derniers modèles</h2>
+    <div class="slider">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+        <img src="./assets/img/bike1.jpg" alt="" srcset="" width="300vw">
+    </div>
 </section>
 <section id="bike_more_info">
     <h3 class="text-center mt-5 mb-3">NOTRE GALERIE PHOTO</h3>
-    <div class="Info">     
-            <div class="more_info">
-                <div class="img_info">
+    <div class="Info">
+        <div class="more_info">
+            <div class="img_info">
                 <img src="./assets/img/bike1.jpg" alt="" srcset="" width="600px" height="400px">
-                </div>
-                <div class="info_text">
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident modi non molestiae alias? Ullam, debitis labore tenetur nam dolore officiis dolorem harum adipisci rem quae alias doloribus minima aperiam suscipit!
-                        rem quae alias doloribus minima aperiam suscipit!
-                    </p>
-                </div>
             </div>
-            <div class="more_info">
-                <div class="info_text">
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident modi non molestiae alias? Ullam, debitis labore tenetur nam dolore officiis dolorem harum adipisci rem quae alias doloribus minima aperiam suscipit!
-                        rem quae alias doloribus minima aperiam suscipit!
-                    </p>
-                </div>
-                <div class="img_info">
+            <div class="info_text">
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident modi non molestiae alias? Ullam, debitis labore tenetur nam dolore officiis dolorem harum adipisci rem quae alias doloribus minima aperiam suscipit!
+                    rem quae alias doloribus minima aperiam suscipit!
+                </p>
+            </div>
+        </div>
+        <div class="more_info">
+            <div class="info_text">
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident modi non molestiae alias? Ullam, debitis labore tenetur nam dolore officiis dolorem harum adipisci rem quae alias doloribus minima aperiam suscipit!
+                    rem quae alias doloribus minima aperiam suscipit!
+                </p>
+            </div>
+            <div class="img_info">
                 <img src="./assets/img/bike1.jpg" alt="" srcset="" width="600px">
-                </div>
             </div>
+        </div>
     </div>
 </section>
-<?php  $content = ob_get_clean()?>
+<?php $content = ob_get_clean() ?>
